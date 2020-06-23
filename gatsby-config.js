67ce904a2628094c15b0080c9a798ba4c2bac476
Ivan Melnychenko <ivmel.ca@gmail.com>
2020-06-23 17:39:51 +0300

    built a sandbox project on GatsbyJS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Ivan Melnychenko's Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@Melnychenko97`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,8 +13,17 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
---
+++ src/markdown-pages/my-first-ama.md
@@ -0,0 +1,9 @@
+---
+title: 'Gatsby Blog Testimony'
+date: '2017-08-21'
+description: 'My Gatsby Blog Testimony :)'
+---
+
+Thank you Gatsby! I like it!.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/LsfwHcHvWtY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
